71170ab48d14ace50f67e6e68fbd0d4d1c25cc42
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-10-23 23:31:53 -0300

    Adicionei algumas matérias para dar inicio
    o dicionário 'Disciplinas' guarda as matérias os turnos, que são identificadas pela chave que vem antes de cada uma delas.

---
+++ main.py
@@ -0,0 +1,25 @@
+''' Imports '''
+
+import os
+
+'''
+Essas displinas são as que estão presentes no Excel que está disponivel para ser vizualizado/baixado na pasta 'arquivo_base' (coloquei apenas algumas matérias). Essas serão às matérias que o usuário podera se matricular.
+'''
+
+disciplinas = {
+    "AAS0050":["Análise e Expressão Textual", "35N34"],
+    "AAS0178":["Sociologia", "24N34"],
+    "AAS0700":["Libras", "24N12"],
+    "AEX0064":["Teoria da Computação", "2N1234"],
+    "AEX0102":["Calculo II", "24N12"],
+    "AEX0106":["Engenharia de Requisitos", "6T2345"],
+    "AEX0149":["Introdução a computação e Sistemas de Informação", "35N12"],
+    "AEX0152":["Princípios de Engenharia de Software", "4N1234"],
+    "AEX0153":["Logica e Matemática Discreta", "2N1234"],
+    "AEX0157":["Fundamentos de Banco de Dados", "4N34", "5N12"],
+    "AEX0158":["Algoritmos e Programação III", "6N1234"],
+    "AEX0159":["Analise e Projeto de Sistemas", "3N12", "5N34"],
+    "AEX0162":["Gerencia de Projetos", "6N1234"],
+    "AEX0163":["Sistemas Operacionais", "4N12", "5N34"],
+    "AEX0171":["Tecnologia e Sociedade", "3N12", "5N34"],
+}
